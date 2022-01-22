cd5a1db1f1142bfafdc573bf1f701c4062e03dc3
Taesub Yun <taesubyun@icloud.com>
2022-01-22 20:00:46 +0900

    style : SectionLoc 스타일드컴포넌트 초안 작성

--- src/Components/SectionLoc.js
+++ src/Components/SectionLoc.js
@@ -2,189 +2,64 @@ import styled from 'styled-components';
 
 const SectionLoc = () => {
     return (
-        <div
-            style={{
-                display: 'flex',
-                padding: '10px 10px',
-                marginTop: '7px',
-                boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
-                borderRadius: '15px',
-            }}
-        >
-            <div className='location_content_left'>
-                <div
-                    style={{
-                        width: '130px',
-                        height: '100%',
-                        boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
-                    }}
-                >
-                    <div
-                        style={{
-                            fontSize: '15px',
-                            padding: '10px 0',
-                            textAlign: 'center',
-                            background: '#ececec',
-                        }}
-                    >
-                        <a
-                            href=''
-                            style={{
-                                textDecorationLine: 'none',
-                                color: 'black',
-                            }}
-                        >
-                            서울
-                        </a>
-                    </div>
-                    <div
-                        style={{
-                            fontSize: '15px',
-                            padding: '20px 0',
-                            textAlign: 'center',
-                            background: '#ececec',
-                        }}
-                    >
-                        <a
-                            href=''
-                            style={{
-                                textDecorationLine: 'none',
-                                color: 'black',
-                            }}
-                        >
-                            인천
-                        </a>
-                    </div>
-                    <div
-                        style={{
-                            fontSize: '15px',
-                            padding: '20px 0',
-                            textAlign: 'center',
-                            background: '#ececec',
-                        }}
-                    >
-                        <a
-                            href=''
-                            style={{
-                                textDecorationLine: 'none',
-                                color: 'black',
-                            }}
-                        >
-                            대구
-                        </a>
-                    </div>
-                    <div
-                        style={{
-                            fontSize: '15px',
-                            padding: '20px 0',
-                            textAlign: 'center',
-                            background: '#ececec',
-                        }}
-                    >
-                        <a
-                            href=''
-                            style={{
-                                textDecorationLine: 'none',
-                                color: 'black',
-                            }}
-                        >
-                            대전
-                        </a>
-                    </div>
-                    <div
-                        style={{
-                            fontSize: '15px',
-                            padding: '20px 0',
-                            textAlign: 'center',
-                            background: '#ececec',
-                        }}
-                    >
-                        <a
-                            href=''
-                            style={{
-                                textDecorationLine: 'none',
-                                color: 'black',
-                            }}
-                        >
-                            경남
-                        </a>
-                    </div>
-                    <div
-                        style={{
-                            fontSize: '15px',
-                            padding: '20px 0',
-                            textAlign: 'center',
-                            background: '#ececec',
-                        }}
-                    >
-                        <a
-                            href=''
-                            style={{
-                                textDecorationLine: 'none',
-                                color: 'black',
-                            }}
-                        >
-                            경북
-                        </a>
-                    </div>
-                    <div
-                        style={{
-                            fontSize: '15px',
-                            padding: '20px 0',
-                            textAlign: 'center',
-                            background: '#ececec',
-                        }}
-                    >
-                        <a
-                            href=''
-                            style={{
-                                textDecorationLine: 'none',
-                                color: 'black',
-                            }}
-                        >
-                            전북
-                        </a>
-                    </div>
-                    <div
-                        style={{
-                            fontSize: '15px',
-                            padding: '20px 0',
-                            textAlign: 'center',
-                            background: '#ececec',
-                        }}
-                    >
-                        <a
-                            href=''
-                            style={{
-                                textDecorationLine: 'none',
-                                color: 'black',
-                            }}
-                        >
-                            전주
-                        </a>
-                    </div>
-                    <div
-                        style={{
-                            fontSize: '15px',
-                            padding: '20px 0',
-                            textAlign: 'center',
-                            background: '#ececec',
-                        }}
-                    >
-                        <a
-                            href=''
-                            style={{
-                                textDecorationLine: 'none',
-                                color: 'black',
-                            }}
-                        >
-                            제주
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <SectionLocContainer>
+            <LeftContent>
+                <LeftContentBox>
+                    <LocationBox>
+                        <LocationName href=''>서울</LocationName>
+                    </LocationBox>
+                    <LocationBox>
+                        <LocationName href=''>경기</LocationName>
+                    </LocationBox>
+                    <LocationBox>
+                        <LocationName href=''>인천</LocationName>
+                    </LocationBox>
+                    <LocationBox>
+                        <LocationName href=''>대전</LocationName>
+                    </LocationBox>
+                    <LocationBox>
+                        <LocationName href=''>대구</LocationName>
+                    </LocationBox>
+                    <LocationBox>
+                        <LocationName href=''>경북</LocationName>
+                    </LocationBox>
+                    <LocationBox>
+                        <LocationName href=''>전북</LocationName>
+                    </LocationBox>
+                    <LocationBox>
+                        <LocationName href=''>제주</LocationName>
+                    </LocationBox>
+                </LeftContentBox>
+            </LeftContent>
+        </SectionLocContainer>
     );
 };
 
 export default SectionLoc;
+
+const SectionLocContainer = styled.div`
+    display: flex;
+    padding: 10px 10px;
+    margin-top: 7px;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    border-radius: 15px;
+`;
+
+const LeftContent = styled.div``;
+const LeftContentBox = styled.div`
+    width: 130px;
+    height: 100%;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+`;
+
+const LocationBox = styled.div`
+    font-size: 15px;
+    padding: 10px 0;
+    text-align: center;
+    background: #ececec;
+`;
+
+const LocationName = styled.a`
+    text-decoration-line: none;
+    color: black;
+`;
